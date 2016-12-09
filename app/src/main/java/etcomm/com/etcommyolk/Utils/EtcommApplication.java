ab4fb44de1c5a3f0fa03462cc27f9e155b0de712
zuohr <zuohr@etcomm.cn>
2016-12-09 10:54:55 +0800

    提交：添加一件退出方法

--- app/src/main/java/etcomm/com/etcommyolk/Utils/EtcommApplication.java
+++ app/src/main/java/etcomm/com/etcommyolk/Utils/EtcommApplication.java
@@ -1,8 +1,11 @@
 package etcomm.com.etcommyolk.Utils;
 
+import android.app.Activity;
 import android.app.Application;
 import android.os.Build;
 import android.view.WindowManager;
+
+import java.util.ArrayList;
 
 /**
  * Created by zuohr on 2016/12/8.
@@ -18,6 +21,32 @@ public class EtcommApplication extends Application {
      * 无故增多网络请求次数，因此添加限定
      */
     public static boolean isFirstSetDefault = true;
+    /**
+     * 添加的打开过的Activity
+     */
+    public static ArrayList<Activity> activities = new ArrayList<Activity>();
+
+    /**
+     * 添加Activity  一键退出调用这个方法
+     *
+     * @param activity
+     */
+    public static void addActivity(Activity activity) {
+        activities.add(activity);
+    }
+
+    /**
+     * Activity 的一键退出
+     */
+    public static void finishActivity() {
+        for (int i = 0; i < activities.size(); i++) {
+            activities.get(i).finish();
+        }
+        /**
+         * 所有activity关闭后，结束进程
+         */
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
 
 
 }
